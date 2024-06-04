b1ac1c31c56060c097be2cba95ed2d64e92f15e1
Yael <maka28684@gmail.com>
2024-06-04 14:41:58 -0700

    qla berga

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,7 +6,7 @@ const HomeScreen = ({ navigation }) => {
   const [pokemon, setPokemon] = useState([]);
 
   useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon?limit=20')
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=300')
       .then(response => {
         setPokemon(response.data.results);
       })
