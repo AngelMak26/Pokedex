9d1718f26e0f334ee1f257c6e94394fabc2df300
Yael <166560482+AngelMak26@users.noreply.github.com>
2024-06-04 23:42:19 -0700

    klk

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
 import axios from 'axios';
 
 const DetailsScreen = ({ route }) => {
@@ -21,14 +21,56 @@ const DetailsScreen = ({ route }) => {
   }
 
   return (
-    <View>
-      <Text>Name: {pokemonDetails.name}</Text>
-      <Text>Height: {pokemonDetails.height}</Text>
-      <Text>Weight: {pokemonDetails.weight}</Text>
-      <Text>Base Experience: {pokemonDetails.base_experience}</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>{pokemonDetails.name.toUpperCase()}</Text>
+      <Image
+        source={{ uri: pokemonDetails.sprites.front_default }}
+        style={styles.image}
+      />
+      <Text style={styles.details}>Height: {pokemonDetails.height}</Text>
+      <Text style={styles.details}>Weight: {pokemonDetails.weight}</Text>
+      <Text style={styles.details}>Base Experience: {pokemonDetails.base_experience}</Text>
+      <Text style={styles.details}>Abilities:</Text>
+      {pokemonDetails.abilities.map((ability, index) => (
+        <Text key={index} style={styles.detailItem}>- {ability.ability.name}</Text>
+      ))}
+      <Text style={styles.details}>Types:</Text>
+      {pokemonDetails.types.map((type, index) => (
+        <Text key={index} style={styles.detailItem}>- {type.type.name}</Text>
+      ))}
+      <Text style={styles.details}>Stats:</Text>
+      {pokemonDetails.stats.map((stat, index) => (
+        <Text key={index} style={styles.detailItem}>- {stat.stat.name}: {stat.base_stat}</Text>
+      ))}
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: 16,
+  },
+  details: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 16,
+  },
+  detailItem: {
+    fontSize: 16,
+  },
+});
 
 export default DetailsScreen;
 
